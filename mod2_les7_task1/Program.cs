315a4fc05c3245393c43600c7b3365e87ae3b795
Szymon Chyc <erressrsch@gmail.com>
2024-01-08 23:05:49 +0100

    Add project files.

---
+++ mod2_les7_task1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace mod2_les7_task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 5;
+
+            if (a == b)
+            {
+                Console.WriteLine($"Variable {a} is equal variable {b}");
+            }
+            else
+            {
+                Console.WriteLine($"Variable {a} is not equal variable {b}");
+            }
+        }
+    }
+}
+
